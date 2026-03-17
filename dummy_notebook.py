652e2e762215cc5a24d8c93dd705400b128696d6
Maaike <maaike@aignostics.com>
2026-03-17 12:00:51 +0100

    try charts

--- dummy_notebook.py
+++ dummy_notebook.py
@@ -4,30 +4,28 @@ __generated_with = "0.21.0"
 app = marimo.App(width="medium")
 
 
-@app.cell(hide_code=True)
+@app.cell
 def _():
     import marimo as mo
 
-    dropdown = mo.ui.dropdown(options=["Show me a cat!", "Show me a dog!"], label="What would you like to see?")
+    dropdown = mo.ui.dropdown(options=["Lines!", "Bars!"], label="What would you like to see?")
     dropdown
     return (dropdown,)
 
 
-@app.cell(hide_code=True)
+@app.cell
 async def _(dropdown):
     import micropip
     await micropip.install("plotly[express]") # <== new package install, not included in marimo env OOB.
+
     import plotly.express as px
+    import pandas as pd
 
-    from PIL import Image
-    from PIL import ImageFile
-
-    ImageFile.LOAD_TRUNCATED_IMAGES=True
-
-    img = 'cat.png' if dropdown.value == "Show me a cat!" else 'dog.png'
-
-    img = Image.open('public/cat.png')
-    fig = px.imshow(img)
+    df = pd.read_csv('public/dummy_data.csv')
+    if dropdown.value == "Lines!":
+        fig = px.line(x = df.x, y = df.y)
+    else:
+        fig = px.bar(x=df.x, y=df.y)
     fig
     return
 
