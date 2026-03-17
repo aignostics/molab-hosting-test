aebf592215eba5675cb6ba81571dfed1bd748934
Maaike <maaike@aignostics.com>
2026-03-17 11:37:03 +0100

    dummy notebook

---
+++ dummy_notebook.py
@@ -0,0 +1,36 @@
+import marimo
+
+__generated_with = "0.21.0"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    dropdown = mo.ui.dropdown(options=["Show me a cat!", "Show me a dog!"], label="What would you like to see?")
+    dropdown
+    return dropdown, mo
+
+
+@app.cell
+def _(dropdown, mo):
+    import plotly.express as px
+    from skimage import io
+
+    loc = mo.notebook_location()
+
+    img = 'cat.png' if dropdown.value == "Show me a cat!" else 'dog.png'
+    img = io.imread('public/' + img)
+    fig = px.imshow(img)
+    fig
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
